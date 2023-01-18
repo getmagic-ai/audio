a66e5234debca2bd9bf12e1ba1a443f8bd162b09
Prathmesh Sadake <prathmeshsadake@gmail.com>
2023-01-18 11:54:44 +0530

    Created signup and signin pages

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -2,6 +2,7 @@ import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import { useState } from "react";
 import {app} from '../../firebase-config'
 import { useRouter } from "next/router";
+
 export default function Signin() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
