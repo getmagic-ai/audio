b6a8bcc998b8c013a338c3b9a1cfca1a81d91932
Prathmesh Sadake <prathmeshsadake@gmail.com>
2023-01-22 08:05:13 +0530

    Created navbar and bottomnavigation

---
+++ Layout/BottomNavigation.js
@@ -0,0 +1,29 @@
+import React from "react";
+import {
+  ArrowTrendingUpIcon,
+  HeartIcon,
+  QueueListIcon,
+  HomeIcon,
+} from "@heroicons/react/24/outline";
+import Link from "next/link";
+
+const BottomNavigation = () => {
+  return (
+    <div className='btm-nav max-w-md mx-auto'>
+      <Link href='/dashboard'>
+        <HomeIcon height={25} />
+      </Link>
+      <Link href='/dashboard/trending-songs' className='active'>
+        <ArrowTrendingUpIcon height={25} />
+      </Link>
+      <Link href='/dashboard/songs'>
+        <QueueListIcon height={25} />
+      </Link>
+      <Link href='/dashboard/my-likes'>
+        <HeartIcon height={25} />
+      </Link>
+    </div>
+  );
+};
+
+export default BottomNavigation;
---
+++ Layout/Navbar.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { UserIcon, HeartIcon } from "@heroicons/react/24/outline";
+
+const Navbar = () => {
+  return (
+    <div className='navbar bg-base-100'>
+      <div className='navbar-start'>
+        <div className='dropdown'>
+          <label tabIndex={0} className='btn btn-ghost btn-circle'>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              className='h-5 w-5'
+              fill='none'
+              viewBox='0 0 24 24'
+              stroke='currentColor'
+            >
+              <path
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                strokeWidth='2'
+                d='M4 6h16M4 12h16M4 18h7'
+              />
+            </svg>
+          </label>
+          <ul
+            tabIndex={0}
+            className='menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 w-52'
+          >
+            <li>
+              <a>Profile</a>
+            </li>
+            <li>
+              <a>Settings</a>
+            </li>
+            <li>
+              <a>Sign Out</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className='navbar-center'>
+        <a className='btn btn-ghost normal-case text-xl'>Audio</a>
+      </div>
+      <div className='navbar-end'>
+        <button className='btn btn-ghost btn-circle'>
+          <div className='indicator'>
+            <UserIcon height={25} />
+          </div>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
