1f7129b35cb33d0966ac1e2a83c01f401b090083
Prathmesh Sadake <prathmeshsadake@gmail.com>
2023-01-22 08:05:38 +0530

    Got react-query working and created trending-songs ui

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,22 @@
 import { AuthProvider } from "@/context/AuthContext";
 import DashboardLayout from "@/layout/dashboard-layout";
 import "@/styles/globals.css";
-import { Inter } from "@next/font/google";
+import { Poppins } from "@next/font/google";
 
 import Head from "next/head";
 import { useRouter } from "next/router";
 import React from "react";
+import { QueryClient, useQuery, QueryClientProvider } from "react-query";
 
-const inter = Inter({ subsets: ["latin"] });
+const poppins = Poppins({
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
+});
 
 const openRoutes = ["/", "/invitation-code", "/auth/signup", "/auth/signin"];
 
+// Created a client
+const queryClient = new QueryClient();
 export default function App({ Component, pageProps }) {
   const router = useRouter();
   return (
@@ -20,9 +26,11 @@ export default function App({ Component, pageProps }) {
       </Head>
       {!openRoutes.includes(router.pathname) ? (
         <AuthProvider>
-          <DashboardLayout className={inter.className}>
-            <Component {...pageProps} />
-          </DashboardLayout>
+          <QueryClientProvider client={queryClient}>
+            <DashboardLayout className={poppins.className}>
+              <Component {...pageProps} />
+            </DashboardLayout>
+          </QueryClientProvider>
         </AuthProvider>
       ) : (
         <Component {...pageProps} />
@@ -30,3 +38,21 @@ export default function App({ Component, pageProps }) {
     </div>
   );
 }
+
+export const fetchAudioData = async () => {
+  // Creating a GET method call here, will be passed as an argument to useQuery elsewhere to get the data
+  console.log(
+    "logging API token from env..." + process.env.NEXT_PUBLIC_API_TOKEN
+  );
+  const res = await fetch(
+    "https://kollaboratenocodb.herokuapp.com/api/v1/db/data/v1/audio/audio_master_dev?limit=100&shuffle=0&offset=0",
+    {
+      method: "GET",
+      headers: {
+        acccept: "application/json",
+        "xc-token": process.env.NEXT_PUBLIC_API_TOKEN,
+      },
+    }
+  );
+  return res.json();
+};
