08a2301e56d53936de409b6b2eb0dd097cf60375
andylnkd <dhalwani1@gmail.com>
2023-01-31 22:24:29 -0800

    added form data to write to API

--- components/WaitlistComponent.jsx
+++ components/WaitlistComponent.jsx
@@ -1,59 +1,83 @@
-export default function WaitlistComponent(props) {
+import { useState } from "react"
+
+export default function WaitlistComponent({ handleWaitlistData }) {
+const [waitilist_name, setWaitlist_name] = useState(null)
+const [waitilist_email, setWaitlist_email] = useState(null)
+const [waitilist_phone, setWaitlist_phone] = useState(null)
+
+const handleNameChange = (e) => setWaitlist_name(e.target.value)
+const handleEmailChange = (e) => setWaitlist_email(e.target.value)
+const handlePhoneChange = (e) => setWaitlist_phone(e.target.value)
+
+
+
+const handleSubmit = (e) => {e.preventDefault()
+  handleWaitlistData({waitilist_name, waitilist_email, waitilist_phone})
+}
+
+
   return (
     <>
       <div className="text-2xl align-middle items-center">
-        
         Please join our waitlist to be notified when we go live!
       </div>
 
       <div className="text-sm align-middle items-center">
-        
         We will use your contact information only to send you the invitation
         code!
       </div>
-      <label
-        htmlFor='waitlist-name'
-        className='block text-sm font-medium text-gray-200'
-      >
-        Name
-      </label>
-      <div className='mt-1'>
-        <input
-          type='waitlist-name'
-          name='waitlist-name'
-          id='waitlist-name'
-          className='bg-gray-700 text-white shadow-sm block w-full sm:text-sm border-gray-300 rounded-md px-4 py-3'
-        />
-      </div>
+      <form onSubmit={handleSubmit}>
+        <label
+          htmlFor="waitlist-name"
+          className="block text-sm font-medium text-gray-200"
+        >
+          Name
+        </label>
+        <div className="mt-1">
+          <input
+            type="waitlist-name"
+            name="waitlist-name"
+            value={waitilist_name}
+            onChange={handleNameChange}
+            id="waitlist-name"
+            className="bg-gray-700 text-white shadow-sm block w-full sm:text-sm border-gray-300 rounded-md px-4 py-3"
+          />
+        </div>
 
-      <label
-        htmlFor='waitlist-email'
-        className='block text-sm font-medium text-gray-200'
-      >
-        Email
-      </label>
-      <div className='mt-1'>
-        <input
-          type='waitlist-email'
-          name='waitlist-email'
-          id='waitlist-email'
-          className='bg-gray-700 text-white shadow-sm block w-full sm:text-sm border-gray-300 rounded-md px-4 py-3'
-        />
-      </div>
-      <label
-        htmlFor='waitlist-phone'
-        className='block text-sm font-medium text-gray-200'
-      >
-        Phone Number
-      </label>
-      <div className='mt-1'>
-        <input
-          type='waitlist-phone'
-          name='waitlisst-phone'
-          id='waitlist-phone'
-          className='bg-gray-700 text-white shadow-sm block w-full sm:text-sm border-gray-300 rounded-md px-4 py-3'
-        />
-      </div>
+        <label
+          htmlFor="waitlist-email"
+          className="block text-sm font-medium text-gray-200"
+        >
+          Email
+        </label>
+        <div className="mt-1">
+          <input
+            type="waitlist-email"
+            name="waitlist-email"
+            value={waitilist_email}
+            onChange={handleEmailChange}
+            id="waitlist-email"
+            className="bg-gray-700 text-white shadow-sm block w-full sm:text-sm border-gray-300 rounded-md px-4 py-3"
+          />
+        </div>
+        <label
+          htmlFor="waitlist-phone"
+          className="block text-sm font-medium text-gray-200"
+        >
+          Phone Number
+        </label>
+        <div className="mt-1">
+          <input
+            type="waitlist-phone"
+            name="waitlist-phone"
+            value={waitilist_phone}
+            onChange={handlePhoneChange}
+            id="waitlist-phone"
+            className="bg-gray-700 text-white shadow-sm block w-full sm:text-sm border-gray-300 rounded-md px-4 py-3"
+          />
+        </div>
+        <button tpye="submit"> Submit </button>
+      </form>
     </>
   );
 }
--- pages/waitlist.jsx
+++ pages/waitlist.jsx
@@ -1,9 +1,16 @@
 import WaitlistComponent from "../components/WaitlistComponent";
 import Link from "next/link";
 import { ArrowRightCircleIcon } from "@heroicons/react/24/outline";
+import { useState } from "react";
+import { data } from "autoprefixer";
 
 export default function Waitlist() {
+  const [formData, setFormData] = useState({}); //initialize with a null object for each new render
+  const postWaitlistData = (data) => {
+    setFormData((prevFormData) => ({ ...prevFormData, ...data }));
+  };
   const handleWaitlistFormSubmit = () => {
+    console.log('in the waitlist component....->' + formData)
     //curl -X 'POST' \
     //   'https://kollaboratenocodb.herokuapp.com/api/v1/db/data/v1/audio/waitlist' \
     //   -H 'accept: application/json' \
@@ -20,7 +27,7 @@ export default function Waitlist() {
 
   return (
     <div className="container max-w-xs mx-auto sm:px-6 lg:px-8 flex flex-col justify-center h-screen">
-      <WaitlistComponent />
+      <WaitlistComponent handleWaitlistData={postWaitlistData} />
       <Link
         href="/app"
         className="max-w-xs mt-2 inline-flex justify-between items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
