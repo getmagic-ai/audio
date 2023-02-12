b96e5846121216727969c5d598c56e18b585c3ff
Prathmesh Sadake <prathmeshsadake@gmail.com>
2023-02-12 20:35:06 +0530

    Removed env and added example file

--- pages/app/trending-songs.js
+++ pages/app/trending-songs.js
@@ -19,11 +19,13 @@ const TrendingSongs = () => {
   const [selectedTab, setSelectedTab] = useState(1);
   const router = useRouter();
   const { currentUser, userData, loading } = useContext(AuthContext);
-  const { data, isInitialLoading, error } = useQuery(/*data is loaded inthe data object*/
+  const { data, isInitialLoading, error } = useQuery(
+    /*data is loaded inthe data object*/
     ["data"],
     fetchAudioData
   ); /*, {staleTime: 10}*/ //stale time isn't really needed, the defaults work well. Keeping it here for reference, can delete it
   // console.log("Hey, just entered the data fethcing part..."); //debugging only
+  console.log(data);
   if (isInitialLoading)
     return (
       <ClipLoader
@@ -40,9 +42,15 @@ const TrendingSongs = () => {
     );
   if (error) return "An error occured in fetching the data from nocodb";
   // console.log(data.list[4]); //debugging only
- const date = new Date();
- const formattedDate = date.toLocaleDateString('en-US', {weekday: 'long'/*, year: 'numeric'*/, month: 'long',day: 'numeric'});
- const openSongDeeplink = (direct_audio_link) => {direct_audio_link? router.push(direct_audio_link): null}
+  const date = new Date();
+  const formattedDate = date.toLocaleDateString("en-US", {
+    weekday: "long" /*, year: 'numeric'*/,
+    month: "long",
+    day: "numeric",
+  });
+  const openSongDeeplink = (direct_audio_link) => {
+    direct_audio_link ? router.push(direct_audio_link) : null;
+  };
   return (
     <div className='bg-black'>
       <h1 className='text-xl font-semibold text-gray-200 mb-8'>
@@ -71,67 +79,79 @@ const TrendingSongs = () => {
         </div>
       </div>
       <ul role='list' className='divide-y divide-gray-200'>
-        {
-        data.list.filter(item => item.channel === (selectedTab === 1?"tiktok":(selectedTab === 2?"instagram":"youtube"))).
-        map((item) => {
-          // console.log(data.list)
-          // console.log('tab selected is ... '+selectedTab)
-          return (
-            <li className='py-3 sm:py-4' key={item.Id}>
-              <div className='flex items-center space-x-4'>
-                <div className='flex-shrink-0'>
-                  <a
-                    href={item.direct_audio_link}
-                    target={"_blank"}
-                    rel='noreferrer noopener'
-                  >
-                    <Avatar
-                      size='36'
-                      round={false}
-                      name={item.artist_name} 
-                      src={item.datasource_metadata?.coverThumb} /*had to add this optional chaining .? item in to avoid a hard error, please fix @PrathmeshSadake */
-                      color={Avatar.getRandomColor("sitebase", [
-                        "red",
-                        "green",
-                        "blue",
-                      ])}
-                    />
-                  </a>
-                </div>
-                <div className='flex-1 min-w-0'   onClick={() => openSongDeeplink(item.direct_audio_link)}>
-                  <p className='text-sm font-medium truncate text-white'>
-                    {item.title}
-                  </p>
-                  <p className='text-xs font-medium truncate text-gray-200'>
-                    {item.artist_name}
-                  </p>
-                </div>
-                <div className='inline-flex items-center space-x-2'>
-                  {parseInt(item.ranking_change) >= 0 ? (
-                    <ArrowTrendingUpIcon height={20} color={"green"} />
-                  ) : parseInt(item.ranking_change) == 0 ? (
-                    <ArrowRightIcon height={20} color={"yellow"} />
-                  ) : (
-                    <ArrowTrendingDownIcon height={20} color={"red"} />
-                  )}
-                  <ArrowUpOnSquareIcon height={20} />
-                  <HeartIcon
-                    className='cursor-pointer'
-                    height={20}
-                    onClick={() => {
-                      currentUser == null && router.push("/auth/signin"); //@PrathmeshSadake can we test if users is signed in? It should add to their favorites list right?
-                    }}
-                  />
-                </div>
-              </div>
-            </li>
-          );
-        })}
+        {data &&
+          data.list
+            .filter(
+              (item) =>
+                item.channel ===
+                (selectedTab === 1
+                  ? "tiktok"
+                  : selectedTab === 2
+                  ? "instagram"
+                  : "youtube")
+            )
+            .map((item) => {
+              // console.log(data.list)
+              // console.log('tab selected is ... '+selectedTab)
+              return (
+                <li className='py-3 sm:py-4' key={item.Id}>
+                  <div className='flex items-center space-x-4'>
+                    <div className='flex-shrink-0'>
+                      <a
+                        href={item.direct_audio_link}
+                        target={"_blank"}
+                        rel='noreferrer noopener'
+                      >
+                        <Avatar
+                          size='36'
+                          round={false}
+                          name={item.artist_name}
+                          src={
+                            item.datasource_metadata?.coverThumb
+                          } /*had to add this optional chaining .? item in to avoid a hard error, please fix @PrathmeshSadake */
+                          color={Avatar.getRandomColor("sitebase", [
+                            "red",
+                            "green",
+                            "blue",
+                          ])}
+                        />
+                      </a>
+                    </div>
+                    <div
+                      className='flex-1 min-w-0'
+                      onClick={() => openSongDeeplink(item.direct_audio_link)}
+                    >
+                      <p className='text-sm font-medium truncate text-white'>
+                        {item.title}
+                      </p>
+                      <p className='text-xs font-medium truncate text-gray-200'>
+                        {item.artist_name}
+                      </p>
+                    </div>
+                    <div className='inline-flex items-center space-x-2'>
+                      {parseInt(item.ranking_change) >= 0 ? (
+                        <ArrowTrendingUpIcon height={20} color={"green"} />
+                      ) : parseInt(item.ranking_change) == 0 ? (
+                        <ArrowRightIcon height={20} color={"yellow"} />
+                      ) : (
+                        <ArrowTrendingDownIcon height={20} color={"red"} />
+                      )}
+                      <ArrowUpOnSquareIcon height={20} />
+                      <HeartIcon
+                        className='cursor-pointer'
+                        height={20}
+                        onClick={() => {
+                          currentUser == null && router.push("/auth/signin"); //@PrathmeshSadake can we test if users is signed in? It should add to their favorites list right?
+                        }}
+                      />
+                    </div>
+                  </div>
+                </li>
+              );
+            })}
       </ul>
     </div>
   );
 };
 
 export default TrendingSongs;
-
-
