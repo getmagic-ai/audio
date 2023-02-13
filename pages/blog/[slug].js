4facbafd443c982766d04e41f68615aec1967b6e
andylnkd <dhalwani1@gmail.com>
2023-02-13 11:44:38 -0800

    the blog is now working with the slug properly inserted

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,50 +1,64 @@
 import { getBlogs } from "./api";
 
-
-
-const blogs = async () => {
-  const result = await getBlogs();
-  console.log(result.data);
-  // return result.data;
-}
+import { useRouter } from "next/router";
 
 
 export const getStaticPaths = async () => {
   const result = await getBlogs();
-  // console.log(result.data.data)
-  // console.log("result is...." + result.data.data[1].attributes.blog_body +' title...' + result.data.data[1].attributes.Title);
   console.log("title is...." + result.data.data[1].attributes.Title);
   console.log(result.data.data[2].id)
   return {
-    paths: result.data.data.map((result) => ({
-      
-      params: { id: toString(result.attributes.id) },
-    })),
+    paths: result.data.data.map((item) => (
+      {params: {slug: item.attributes.slug}}
+    )),
     fallback: false,
   };
 };
 
 export const getStaticProps = async ({ params }) => {
+  const result = await getBlogs();
+  console.log(result.data.data[1].attributes)
   try {
-    console.log("Here are the blogs .." + blogs.data);
+    console.log("Here are the blogs .." + result.data.data[1].attributes.blog_body);
   } catch (error) {
     console.log("error is... in the blog page " + error);
   }
-  const result = blogs.result.json();
+  
   return {
     props: {
-      data: result.data.body,
+      data: result.data.data,
+      // data: result.data.data.attributes.blog_body,
     },
   };
 };
 
+
+
 export default function Post({ data }) {
+  const router = useRouter();
+  const { slug } = router.query;
+
+  const selectedBlog = data.find((blog) => blog.attributes.slug === slug);
+
   return (
     <>
-      <div>{data}</div>
+      {selectedBlog && (
+        <div className="text-white">{selectedBlog.attributes.blog_body}</div>
+      )}
     </>
   );
 }
+
+// export default function Post({ data }) {
+//   console.log(data)
+  
+//       data.map((item) => 
+//       {
+//         return (
+//         <div className="text-white">{item.attributes.blog_body}</div>
+//         );
+//       }
+// }
 
 // define get static paths -- in this one , iterate through teh returned value from strapi and map over it to create the arrya with the fallback option
 // define get staticprops  -- in this one, get the data back and return it as props
