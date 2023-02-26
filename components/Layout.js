b4596817c4f85dec91fe0ee6087522333e47a1b0
Prathmesh Sadake <prathmeshsadake@gmail.com>
2023-02-26 19:58:07 +0530

    made layut responsive

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,10 @@
-import Navbar from "./Navbar";
+import { Navbar } from "./Navbar";
 
 export default function AppLayout({ children }) {
   return (
-    <div className='max-w-5xl min-h-screen mx-auto bg-black'>
+    <div className='min-h-screen bg-black'>
       <Navbar />
-      <main className='flex-1'>
+      <main className='mx-auto max-w-5xl flex-1 pt-8'>
         <div className='py-6 pb-24 px-4 sm:px-6'>{children}</div>
       </main>
     </div>
