01c5d5db9b3caa71b5882cfefeeea65c836e7627
Prathmesh Sadake <prathmeshsadake@gmail.com>
2023-03-04 13:11:09 +0530

    Added accessibility for modal

--- pages/app/trending-songs.js
+++ pages/app/trending-songs.js
@@ -7,6 +7,7 @@ import {
   ArrowTrendingDownIcon,
   ArrowRightIcon,
 } from "@heroicons/react/24/outline";
+import { ShareSocial } from "react-share-social";
 import { HeartIcon as HeartIconSolid } from "@heroicons/react/24/solid";
 import { fetchAudioData } from "@/pages/_app";
 import { ClipLoader } from "react-spinners";
@@ -16,6 +17,18 @@ import Avatar from "react-avatar";
 import Link from "next/link";
 import { deleteDoc, doc, getDoc, setDoc } from "firebase/firestore";
 import { db } from "@/config/firebase-config";
+import Modal from "react-modal";
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+  },
+};
+Modal.setAppElement("#root");
 
 const handleLike = async (userId, audioId) => {
   // Add a new document in collection "likes"
@@ -45,6 +58,7 @@ const getHasUserLiked = async (userId, audioId) => {
 
 const TrendingSongs = () => {
   const [selectedTab, setSelectedTab] = useState(1);
+  const [modalIsOpen, setIsOpen] = React.useState(false);
   const router = useRouter();
   const { currentUser, userData, loading } = useContext(AuthContext);
   const { data, isInitialLoading, error } = useQuery(
@@ -81,8 +95,53 @@ const TrendingSongs = () => {
   //   direct_audio_link ? router.push(direct_audio_link) : null;
   // };
 
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
   return (
-    <div className='bg-black'>
+    <div className='bg-black relative'>
+      <div>
+        <button onClick={openModal}>Open Modal</button>
+        <Modal
+          isOpen={modalIsOpen}
+          onRequestClose={closeModal}
+          style={customStyles}
+        >
+          <button onClick={closeModal}>close</button>
+          <ShareSocial
+            url='url_to_share.com'
+            socialTypes={[
+              "facebook",
+              "twitter",
+              "whatsapp",
+              "telegram",
+            ]}
+            onSocialButtonClicked={(data) => console.log(data)}
+            style={{
+              root: {
+                background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
+                borderRadius: 3,
+                border: 0,
+                boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
+                color: "white",
+              },
+              copyContainer: {
+                border: "1px solid blue",
+                background: "rgb(0,0,0,0.7)",
+              },
+              title: {
+                color: "aquamarine",
+                fontStyle: "italic",
+              },
+            }}
+          />
+        </Modal>
+      </div>
       <h1 className='text-xl font-semibold text-gray-200 mb-8'>
         Trending Audio for {formattedDate.toString()}!
       </h1>
@@ -177,7 +236,7 @@ const TrendingSongs = () => {
                       ) : (
                         <ArrowTrendingDownIcon height={20} color={"red"} />
                       )}
-                      <ArrowUpOnSquareIcon height={20} />
+                      <ArrowUpOnSquareIcon height={20} onClick={openModal} />
                       {!currentUser ? (
                         <HeartIcon
                           className='cursor-pointer'
