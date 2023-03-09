285699f799a3b2fd40cd59bacb9b42ea8028ae45
Prathmesh Sadake <prathmeshsadake@gmail.com>
2023-03-09 21:33:56 +0530

    Added footer to layout

--- components/Layout.js
+++ components/Layout.js
@@ -1,3 +1,4 @@
+import Footer from "./Footer";
 import { Navbar } from "./Navbar";
 
 export default function AppLayout({ children }) {
@@ -5,8 +6,9 @@ export default function AppLayout({ children }) {
     <div className='min-h-screen bg-black'>
       <Navbar />
       <main className='mx-auto max-w-5xl flex-1 pt-8'>
-        <div className='py-6 pb-24 px-4 sm:px-6'>{children}</div>
+        <div className='py-6 px-4 sm:px-6'>{children}</div>
       </main>
+      <Footer />
     </div>
   );
 }
